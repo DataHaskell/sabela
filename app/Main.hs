1661c848e961a5e86898104943195889a7ab3322
jireh <daikonradish@users.noreply.github.com>
2026-02-24 23:25:07 +0800

    fix import (#2)

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Monad (when)
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import Network.Wai.Handler.Warp (run)
 import Sabela.Handlers (setupReactive)
@@ -40,7 +41,7 @@ start port staticDir workDir = do
             exitFailure
         else do
             indexExists <- doesFileExist (staticDir ++ "/index.html")
-            Control.Monad.when (not indexExists) $ putStrLn $ "Warning: " ++ staticDir ++ "/index.html not found"
+            when (not indexExists) $ putStrLn $ "Warning: " ++ staticDir ++ "/index.html not found"
 
             putStrLn $ "Serving static files from: " ++ staticDir
 
