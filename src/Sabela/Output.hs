cad26e9fbd0dcc7a8c2a4880a3920573c1745967
Michael Chavinda <mschavinda@gmail.com>
2026-02-27 12:59:22 -0800

    fix: Actually a whole lot of examples could use new lining.

--- src/Sabela/Output.hs
+++ src/Sabela/Output.hs
@@ -48,32 +48,32 @@ builtinExamples =
         "Fibonacci"
         "Lazy infinite list"
         "Basics"
-        "let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)\nmapM_ print (take 15 fibs)"
+        "let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)\n\nmapM_ print (take 15 fibs)"
     , Example
         "List comprehension"
         "Pythagorean triples"
         "Basics"
-        "let triples = [(a,b,c) | c <- [1..20], b <- [1..c], a <- [1..b], a*a + b*b == c*c]\nprint triples"
+        "let triples = [(a,b,c) | c <- [1..20], b <- [1..c], a <- [1..b], a*a + b*b == c*c]\n\nprint triples"
     , Example
         "Map & Filter"
         "Higher-order functions"
         "Basics"
-        "let xs = [1..20]\nprint $ filter even $ map (^2) xs"
+        "let xs = [1..20]\n\nprint $ filter even $ map (^2) xs"
     , Example
         "Working with Text"
         "Text manipulation with the text library"
         "Libraries"
-        "-- cabal: build-depends: text\nimport qualified Data.Text as T\nimport qualified Data.Text.IO as TIO\n\nlet msg = T.pack \"Hello, World!\"\nTIO.putStrLn $ T.toUpper msg\nTIO.putStrLn $ T.reverse msg\nprint $ T.words msg"
+        "-- cabal: build-depends: text\nimport qualified Data.Text as T\nimport qualified Data.Text.IO as TIO\n\nlet msg = T.pack \"Hello, World!\"\n\nTIO.putStrLn $ T.toUpper msg\n\nTIO.putStrLn $ T.reverse msg\n\nprint $ T.words msg"
     , Example
         "HTTP Request"
         "Fetch a URL with http-conduit"
         "Libraries"
-        "-- cabal: build-depends: http-conduit, bytestring\nimport Network.HTTP.Simple\nimport qualified Data.ByteString.Lazy.Char8 as L8\n\nresponse <- httpLBS \"http://httpbin.org/get\"\nL8.putStrLn $ getResponseBody response"
+        "-- cabal: build-depends: http-conduit, bytestring\nimport Network.HTTP.Simple\nimport qualified Data.ByteString.Lazy.Char8 as L8\n\nresponse <- httpLBS \"http://httpbin.org/get\"\n\nL8.putStrLn $ getResponseBody response"
     , Example
         "JSON Parsing"
         "Decode JSON with aeson"
         "Libraries"
-        "-- cabal: build-depends: aeson, text, bytestring\n{-# LANGUAGE DeriveGeneric #-}\nimport Data.Aeson\nimport GHC.Generics\nimport qualified Data.ByteString.Lazy.Char8 as L8\n\ndata Person = Person { name :: String, age :: Int } deriving (Show, Generic)\ninstance FromJSON Person\n\nlet json = L8.pack \"{\\\"name\\\": \\\"Alice\\\", \\\"age\\\": 30}\"\nprint (decode json :: Maybe Person)"
+        "-- cabal: build-depends: aeson, text, bytestring\n{-# LANGUAGE DeriveGeneric #-}\nimport Data.Aeson\nimport GHC.Generics\nimport qualified Data.ByteString.Lazy.Char8 as L8\n\ndata Person = Person { name :: String, age :: Int } deriving (Show, Generic)\ninstance FromJSON Person\n\nlet json = L8.pack \"{\\\"name\\\": \\\"Alice\\\", \\\"age\\\": 30}\"\n\nprint (decode json :: Maybe Person)"
     , Example
         "HTML Output"
         "Render rich HTML output"
@@ -93,15 +93,15 @@ builtinExamples =
         "Concurrent IO"
         "Async with threads"
         "Advanced"
-        "import Control.Concurrent\nimport Control.Monad\n\nmv <- newMVar (0 :: Int)\nlet inc = modifyMVar_ mv (\\n -> pure (n+1))\nts <- forM [1..100] (\\_ -> forkIO inc)\nmapM_ (\\_ -> threadDelay 1000) ts\nthreadDelay 50000\nresult <- readMVar mv\nputStrLn $ \"Counter: \" ++ show result"
+        "import Control.Concurrent\nimport Control.Monad\n\nmv <- newMVar (0 :: Int)\nlet inc = modifyMVar_ mv (\\n -> pure (n+1))\n\nts <- forM [1..100] (\\_ -> forkIO inc)\n\nmapM_ (\\_ -> threadDelay 1000) ts\nthreadDelay 50000\nresult <- readMVar mv\n\nputStrLn $ \"Counter: \" ++ show result"
     , Example
         "QuickCheck"
         "Property-based testing"
         "Advanced"
-        "-- cabal: build-depends: QuickCheck\nimport Test.QuickCheck\n\nlet prop_reverse xs = reverse (reverse xs) == (xs :: [Int])\nquickCheck prop_reverse\n\nlet prop_sort_length xs = length (filter even xs) + length (filter odd xs) == length (xs :: [Int])\nquickCheck prop_sort_length"
+        "-- cabal: build-depends: QuickCheck\nimport Test.QuickCheck\n\nlet prop_reverse xs = reverse (reverse xs) == (xs :: [Int])\nquickCheck prop_reverse\n\nlet prop_sort_length xs = length (filter even xs) + length (filter odd xs) == length (xs :: [Int])\n\nquickCheck prop_sort_length"
     , Example
         "File I/O"
         "Read and write files"
         "Advanced"
-        "writeFile \"/tmp/sabela-test.txt\" \"Hello from Sabela!\\nLine two.\\n\"\ncontents <- readFile \"/tmp/sabela-test.txt\"\nputStrLn contents\nputStrLn $ \"Lines: \" ++ show (length (lines contents))"
+        "writeFile \"/tmp/sabela-test.txt\" \"Hello from Sabela!\\nLine two.\\n\"\ncontents <- readFile \"/tmp/sabela-test.txt\"\n\nputStrLn contents\n\nputStrLn $ \"Lines: \" ++ show (length (lines contents))"
     ]
